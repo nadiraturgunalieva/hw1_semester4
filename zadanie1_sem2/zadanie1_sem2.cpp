ff2cec2be2ea8cc93cc7defee18aff493ba01071
Надира Тургуналиева <1132226224@pfur.ru>
2024-02-15 02:48:29 +0300

    //// end

--- zadanie1_sem2/zadanie1_sem2.cpp
+++ zadanie1_sem2/zadanie1_sem2.cpp
@@ -70,11 +70,47 @@ void sellProduct(dispenserType& dispenser, cashRegister& cashReg) {
         std::cin >> amount;
     }
     dispenser.makeSale();
-
+    cashReg.acceptAmount(dispenser.getCost());
+    std::cout << "Вы внесли: " << amount << " руб.\n";
+    amount -= dispenser.getCost();
+    std::cout << "Ваша сдача: " << amount << " руб.\n";
+    std::cout << "Продукт выдан.\n";
     
 }
-int main()
-{
+int main() {
     setlocale(LC_ALL, "Rus");
-    std::cout << "\n";
+
+    dispenserType candy(2, 67);
+    dispenserType chips(23, 120);
+    dispenserType gum(78, 10);
+    dispenserType cookies(50, 45);
+
+    cashRegister cashReg;
+
+    int choice;
+
+    while (true) {
+        showSelection();
+        std::cin >> choice;
+
+        switch (choice) {
+        case 1:
+            sellProduct(candy, cashReg);
+            break;
+        case 2:
+            sellProduct(chips, cashReg);
+            break;
+        case 3:
+            sellProduct(gum, cashReg);
+            break;
+        case 4:
+            sellProduct(cookies, cashReg);
+            break;
+        default:
+            std::cout << "Некорректный выбор. Пожалуйста, попробуйте ещё раз. \n";
+            break;
+        }
+    }
+
+    return 0;
 }
